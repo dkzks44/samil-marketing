ca656761b27ebab6ec2b97cda4700011ee3c51f4
dkzks44 <dkzks44@naver.com>
2023-01-17 16:45:30 +0900

    크롤링 에러 수정

--- application.py
+++ application.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 from crawling.crawling import crawlSet, title
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/result')
+@application.route('/result')
 def search():
     keyword = request.args.get("keyword")
     data = []
@@ -16,9 +16,9 @@ def search():
     return render_template('result.html', keyword=keyword, getting=getting, title=title)
 
 
-@app.route('/login')
+@application.route('/login')
 def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=5000, debug=True)
+    application.run("0.0.0.0", port=5000, debug=True)
